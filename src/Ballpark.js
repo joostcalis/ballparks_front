c3c9c549ba47716506a0d55863121234f458e0b8
joostcalis <joost_calis@hotmail.com>
2016-03-02 00:54:13 +0100

    couple of small changes

--- src/Ballpark.js
+++ src/Ballpark.js
@@ -36,13 +36,13 @@ class Ballpark extends React.Component {
      <div className="cont">
       <div className="r-header">
         <div className="column1">
-          <h3>{this.state.ballpark.league}</h3>
+          <h2>{this.state.ballpark.league}</h2>
         </div>
         <div className="column2">
           <h1>{this.state.ballpark.name} ({this.state.average_rating})</h1>
         </div>
         <div className="column3">
-          <h3>{this.state.ballpark.team}</h3>
+          <h2>{this.state.ballpark.team}</h2>
         </div>
       </div>
       <div className="r">
